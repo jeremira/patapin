3554564df38ca8af5774b91d8d2d6dbaa3eb44f9
jeremira <jmiraille@gmail.com>
2019-02-20 09:41:59 +0100

    openings/appoinments scopes

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,7 @@
 class Event < ApplicationRecord
 
-  
+  # Scopes
+  scope :openings,     ->() { where(kind: "opening") }
+  scope :appointments, ->() { where(kind: "appointment") }
 
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -4,12 +4,69 @@ RSpec.describe Event, type: :model do
   let(:opening) {create :event, :opening}
   let(:appointment) {create :event, :appointment}
 
-  describe "factories" do
+  describe "Factories" do
     it "has a valid apointment factory" do
       expect(appointment).to be_valid
     end
     it "has a valid opening factory" do
       expect(opening).to be_valid
+    end
+  end
+
+  describe "Scopes" do
+    describe ".openings" do
+      let(:opening_scope) {Event.openings}
+
+      context "when no events" do
+        it "return an active record relation"  do
+          expect(opening_scope).to eq []
+        end
+      end
+
+      context "with no opening kind events" do
+        before :each { create_list :event, 3, :appointment}
+
+        it "return an active record relation"  do
+          expect(opening_scope).to eq []
+        end
+      end
+      context "with opening kind event" do
+        before :each do
+          create_list :event, 3, :opening
+          create_list :event, 3, :appointment
+        end
+
+        it "return an active record relation"  do
+          expect(opening_scope).to eq Event.where(kind: "opening")
+        end
+      end
+    end
+    describe ".appointments" do
+      let(:appointment_scope) {Event.appointments}
+
+      context "when no events" do
+        it "return an active record relation"  do
+          expect(appointment_scope).to eq []
+        end
+      end
+
+      context "with no appointments kind events" do
+        before :each { create_list :event, 3, :opening}
+
+        it "return an active record relation"  do
+          expect(appointment_scope).to eq []
+        end
+      end
+      context "with appointments kind event" do
+        before :each do
+          create_list :event, 3, :opening
+          create_list :event, 3, :appointment
+        end
+
+        it "return an active record relation"  do
+          expect(appointment_scope).to eq Event.where(kind: "appointment")
+        end
+      end
     end
   end
 end
