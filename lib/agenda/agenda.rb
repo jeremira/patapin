71362791c40fbe6335a74b75924a9b2467715ed2
jeremira <jmiraille@gmail.com>
2019-02-20 10:19:07 +0100

    opened? method ok

--- lib/agenda/agenda.rb
+++ lib/agenda/agenda.rb
@@ -6,5 +6,14 @@ class Agenda
     @appointments = params[:appointments]  || []
     @slot_extent  = params[:slot_extent]   || 30*60
   end
-  
+
+  #
+  # Ensure an opening exist at a specified datetime
+  #
+  def opened?(datetime)
+    #@openings.find { |opening| opening.day == date.day &&&&&&  opening.start_at..opening.end_at == date}
+    @openings.find do |opening|
+      datetime.between? opening.starts_at, opening.ends_at
+    end
+  end
 end
--- spec/lib/agenda_spec.rb
+++ spec/lib/agenda_spec.rb
@@ -1,8 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Agenda do
-let(:my_agenda) { Agenda.new(agenda_params) }
-  describe "#intialize" do
+  let(:my_agenda) { Agenda.new(agenda_params) }
+  let(:agenda_params) do
+    {
+      openings: Event.openings,
+      appointments: Event.appointments
+    }
+  end
+
+  before :each do
+    freezed_time = Time.zone.now
+    allow(Time.zone).to receive(:now) {freezed_time}
+  end
+
+  describe "#initialize" do
 
     it "instanciate" do
       expect(Agenda.new).to be_an Agenda
@@ -29,35 +41,78 @@ let(:my_agenda) { Agenda.new(agenda_params) }
       it "setup slot extent to a 30min delay" do
         expect(my_agenda.slot_extent).to eq 30*60
       end
+    end
+    context "with params" do
+      let(:agenda_params) do
+        {
+          openings: create_list(:event, 3, :opening),
+          appointments: create_list(:event, 3, :appointment),
+          slot_extent: 35*60
+        }
+      end
 
-      context "with params" do
-        let(:agenda_params) do
-          {
-            openings: create_list(:event, 3, :opening),
-            appointments: create_list(:event, 3, :appointment),
-            slot_extent: 35*60
-          }
-        end
+      it "expose openings" do
+        expect(my_agenda.openings).to be_an Array
+      end
+      it "setup openings with passed events" do
+        expect(my_agenda.openings).to eq Event.openings
+      end
+      it "expose appointments" do
+        expect(my_agenda.appointments).to be_an Array
+      end
+      it "setup appointments with passed events" do
+        expect(my_agenda.appointments).to eq Event.appointments
+      end
+      it "expose slot extent" do
+        expect(my_agenda.slot_extent).to be_an Integer
+      end
+      it "setup slot extent to a 30min delay" do
+        expect(my_agenda.slot_extent).to eq 35*60
+      end
+    end
+  end
 
-        it "expose openings" do
-          expect(my_agenda.openings).to be_an Array
+  describe "#opened?" do
+
+    context "with recurring event" do
+    end
+
+    context "with no recurring events" do
+      let(:on_time_opening) {create :event, :opening, starts_at: 1.hour.ago,   ends_at: 1.hour.since}
+      let(:past_opening)    {create :event, :opening, starts_at: 2.hour.ago,   ends_at: 1.hour.ago}
+      let(:futur_opening)   {create :event, :opening, starts_at: 1.hour.since, ends_at: 2.hour.since}
+      let(:on_test_function)  {my_agenda.opened? Time.zone.now}
+
+      context "when no openings exist" do
+        it "return nil" do
+          expect(on_test_function).to be nil
         end
-        it "setup openings with passed events" do
-          expect(my_agenda.openings).to eq Event.openings
+      end
+
+      context "when no openings match the requested date" do
+        before :each do
+          past_opening
+          futur_opening
         end
-        it "expose appointments" do
-          expect(my_agenda.openings).to be_an Array
+        it "return nil" do
+          expect(on_test_function).to be nil
         end
-        it "setup appointments with passed events" do
-          expect(my_agenda.openings).to eq Event.appointments
+      end
+
+      context "when an opening match the requested date" do
+        before :each do
+          past_opening
+          futur_opening
+          on_time_opening
         end
-        it "expose slot extent" do
-          expect(my_agenda.slot_extent).to be_an Integer
+        it "return an Event" do
+          expect(on_test_function).to be_an Event
         end
-        it "setup slot extent to a 30min delay" do
-          expect(my_agenda.openings).to eq 35*60
+        it "return an valid opening" do
+          expect(on_test_function).to eq on_time_opening
         end
       end
     end
+
   end
 end
